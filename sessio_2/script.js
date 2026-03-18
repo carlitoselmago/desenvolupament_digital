91f28011c380486d028ff8394615afda45346e94
carlitoselmago <ccarbonell@gmail.com>
2026-03-18 01:02:41 +0100

    neteja codi

--- sessio_2/script.js
+++
@@ -1,9 +0,0 @@
-/*
-var frase = ["Hola que tal","Bon dia","bona nit"];
-var i = Math.floor(Math.random() * frase.length);
-//alert(frase[i]);
-
-var holder = document.getElementById("frase");
-holder.innerHTML =frase[i];
-*/
-
